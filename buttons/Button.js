d685b51e2c80f31fb1e2e5104ea2de5d6b756798
Gabe Lyu <no6xichengavenue@gmail.com>
2020-06-01 01:18:10 +0100

    Tested button. Removed useless stuff

--- buttons/Button.js
+++ buttons/Button.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function Button() {
-  return <div></div>;
+export default function Button(props) {
+  return (
+    <div className="button">
+      <button
+        onClick={props.onButtonClick}
+        disabled={props.disabled}
+        data-testid="button"
+      >
+        {props.text}
+      </button>
+    </div>
+  );
 }
--- buttons/Button.test.js
+++ buttons/Button.test.js
@@ -1,1 +1,85 @@
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { render } from "@testing-library/react";
 
+import Button from "./Button";
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test("Should display", () => {
+  act(() => {
+    render(<Button text="Roar" />, container);
+  });
+  const button = document.querySelector("[data-testid=button]");
+  expect(button.innerHTML).toBe("Roar");
+});
+
+test("Should click", () => {
+  let onClick = jest.fn();
+
+  act(() => {
+    render(<Button text="Roar" onButtonClick={onClick} />, container);
+  });
+  const button = document.querySelector("[data-testid=button]");
+  for (let i = 0; i < 3; i++) {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  }
+  expect(onClick).toHaveBeenCalledTimes(3);
+});
+
+test("Should prevent double submission", () => {
+  let rerender = null;
+
+  let disabled = false;
+  let apiCall = () =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 5000);
+    });
+  let onClick = jest.fn(() => {
+    disabled = true;
+    rerender(
+      <Button text="Roar" onButtonClick={onClick} disabled={disabled} />,
+      container
+    );
+    apiCall().then(() => {
+      disabled = false;
+      rerender(
+        <Button text="Roar" onButtonClick={onClick} disabled={disabled} />,
+        container
+      );
+    });
+  });
+
+  act(() => {
+    let a = render(
+      <Button text="Roar" onButtonClick={onClick} disabled={disabled} />,
+      container
+    );
+    rerender = a.rerender;
+  });
+  let button = document.querySelector("[data-testid=button]");
+  expect(button.disabled).toBeFalsy();
+
+  for (let i = 0; i < 3; i++) {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  }
+  expect(onClick).toHaveBeenCalledTimes(1);
+  expect(button.disabled).toBeTruthy();
+  setTimeout(() => {
+    expect(button.disabled).toBeFalsy();
+  }, 5000);
+});
